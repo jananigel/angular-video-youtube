720496423a0c5fd56a2604b205e26dcedaaa71c1
jananigel <jananigel@hotmail.com>
2020-09-02 23:30:18 +0800

    feat: created main layout

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home.routing';
+import { LayoutModule } from '../components/layouts/layout.module';
 import { HomeListComponent } from './list/home-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    LayoutModule
   ],
   declarations: [HomeListComponent],
 })
